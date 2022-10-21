0d726badf696622552f729c932d00c276d4efb48
Christopherchinia <chinia214@gmail.com>
2022-10-21 04:44:53 +0300

    [:setup]removing unneccessary files from the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-   <div></div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
+
+
