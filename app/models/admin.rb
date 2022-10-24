f501c11ee2ed091567cf03965b3ed9539e3e4448
muliro <mulirokhaemba@gmail.com>
2022-10-24 21:02:15 +0300

    [:chore] Adding validation to admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,11 @@
+
+
+
+
+
+
 class Admin < ApplicationRecord
+    has_secure_password
+    validates :full_name, presence: true, uniqueness: true
+    validates :password, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_24_121435) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_24_175305) do
+  create_table "admins", force: :cascade do |t|
+    t.string "full_name"
+    t.string "password"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
