4ebff49c223bf2249c6c4306dddd58ede4919d83
Eugene <eugeneayonga@gmail.com>
2022-10-24 15:05:25 +0300

    Imported SignUp.js into App.js, built SignUp route

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NavBar />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </div>
   );
