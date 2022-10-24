568481c253b085d58c68dec26f348a3808316685
Eugene <eugeneayonga@gmail.com>
2022-10-24 17:41:31 +0300

    Refactored Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,9 +4,9 @@ function Home() {
   return (
     <div>
         <h1>PBMW</h1>
-        <img alt="PBMW Logo" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.englandautomotiveservice.com%2F&psig=AOvVaw1eL36HZ7CJe3FxZEOrZopt&ust=1666679351701000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCJC5iNKe-PoCFQAAAAAdAAAAABAH" />
     </div>
   )
 }
+
 
 export default Home;
