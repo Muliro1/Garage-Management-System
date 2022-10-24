8d47a1bc4402815f02fecef61200ea0bf9e5a9bb
muliro <mulirokhaemba@gmail.com>
2022-10-24 14:53:54 +0300

    add a vehicle model

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+
+
+class User < ApplicationRecord
+    has_secure_password
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true, uniqueness: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
+
+
+
+
+
+
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
