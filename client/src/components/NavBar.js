ef32fc4003a91fc9aec92797244cf07989f5200c
Eugene <eugeneayonga@gmail.com>
2022-10-24 12:08:31 +0300

    Deleted fragment tags

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,33 +4,33 @@ import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <>
-        <div className='NavBar'>
-            <NavLink to="/">
-            <button className="button">Home</button>
-            </NavLink>
+    
+    <div className='NavBar'>
+        <NavLink to="/">
+        <button className="button">Home</button>
+        </NavLink>
 
-            <NavLink to="/about">
-                <button className="button">About</button>
-            </NavLink>
+        <NavLink to="/about">
+            <button className="button">About</button>
+        </NavLink>
 
-            <NavLink to="/Services">
-                <button className="button">Services</button>
-            </NavLink>
+        <NavLink to="/Services">
+            <button className="button">Services</button>
+        </NavLink>
 
-            <NavLink to="/contact">
-                <button className="button">Contact</button>
-            </NavLink>
+        <NavLink to="/contact">
+            <button className="button">Contact</button>
+        </NavLink>
 
-            <NavLink to="/login">
-                <button className="button">Login</button>
-            </NavLink>
+        <NavLink to="/login">
+            <button className="button">Login</button>
+        </NavLink>
 
-            <NavLink to="/signup">
-                <button className="button">Sign Up</button>
-            </NavLink>
-        </div>
-    </>
+        <NavLink to="/signup">
+            <button className="button">Sign Up</button>
+        </NavLink>
+    </div>
+    
   );
 }
 
