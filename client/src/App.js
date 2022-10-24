ce0eb305bb59463565f7a1bc4c122f4a0fb2a342
Eugene <eugeneayonga@gmail.com>
2022-10-24 14:46:32 +0300

    Imported Login.js into App.js, built login route

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NavBar />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
