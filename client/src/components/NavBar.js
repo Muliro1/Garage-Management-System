8e7f2d4acd67baa5cc80bd5309a543b18fe4602a
Eugene <eugeneayonga@gmail.com>
2022-10-25 13:39:37 +0300

    Built NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,47 +1,102 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+// import React from 'react';
+// import { NavLink } from 'react-router-dom';
 
 
-function NavBar() {
+// function NavBar() {
+//   return (
+    
+//     <div className='NavBar'>
+//         <h1>PBMW</h1>
+//         <NavLink to="/">
+//             <button className="button">Home</button>
+//         </NavLink>
+
+//         <NavLink to="/about">
+//             <button className="button">About</button>
+//         </NavLink>
+
+//         <NavLink to="/user">
+//             <button className="button">User</button>
+//         </NavLink>
+
+//         <NavLink to="/guard">
+//             <button className="button">Guard</button>
+//         </NavLink>
+
+//         <NavLink to="/admin">
+//             <button className="button">Admin</button>
+//         </NavLink>
+
+//         <NavLink to="/technician">
+//             <button className="button">Technician</button>
+//         </NavLink>
+
+//         <NavLink to="/contact">
+//             <button className="button">Contact</button>
+//         </NavLink>
+
+//         <NavLink to="/login">
+//             <button className="button">Login</button>
+//         </NavLink>
+
+//         <NavLink to="/signup">
+//             <button className="button">Sign Up</button>
+//         </NavLink>
+//     </div>
+    
+//   );
+// }
+
+
+// export default NavBar;
+
+
+
+
+
+import React, { useState } from "react"
+import "./navbar.css"
+import { Link } from "react-router-dom"
+import { FaBars } from "react-icons/fa"
+import { ImCross } from "react-icons/im"
+
+const Navbar = () => {
+  const [Mobile, setMobile] = useState(false)
   return (
-    
-    <div className='NavBar'>
-        <h1>PBMW</h1>
-        <NavLink to="/">
-            <button className="button">Home</button>
-        </NavLink>
-
-        <NavLink to="/user">
-            <button className="button">User</button>
-        </NavLink>
-
-        <NavLink to="/guard">
-            <button className="button">Guard</button>
-        </NavLink>
-
-        <NavLink to="/admin">
-            <button className="button">Admin</button>
-        </NavLink>
-
-        <NavLink to="/technician">
-            <button className="button">Technician</button>
-        </NavLink>
-
-        <NavLink to="/contact">
-            <button className="button">Contact</button>
-        </NavLink>
-
-        <NavLink to="/login">
-            <button className="button">Login</button>
-        </NavLink>
-
-        <NavLink to="/signup">
-            <button className="button">Sign Up</button>
-        </NavLink>
-    </div>
-    
-  );
+    <>
+      <nav className='navbar'>
+        <h3 className='logo'>Logo</h3>
+        {/*
+        if large screen ma xa bhane Mobile add huxa
+        if mobile screen ma xa bhane nav-links-mobile add huxa
+        */}
+        <ul className={Mobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}>
+          <Link to='/' className='home'>
+            <li>Home</li>
+          </Link>
+          <Link to='/about' className='about'>
+            <li>About</li>
+          </Link>
+          <Link to='/services' className='services'>
+            <li>Services</li>
+          </Link>
+          <Link to='/skills' className='skills'>
+            <li>Skills</li>
+          </Link>
+          <Link to='/contact' className='home'>
+            <li>contact</li>
+          </Link>
+        </ul>
+        {/* 
+        whenever we click on button = setMobile(!Mobile) ==  is mobile oppsite to setMobile 
+        */}
+        <button className='mobile-menu-icon' onClick={() => setMobile(!Mobile)}>
+          {Mobile ? <ImCross /> : <FaBars />}
+        </button>
+      </nav>
+    </>
+  )
 }
 
 
-export default NavBar;
+export default Navbar;
