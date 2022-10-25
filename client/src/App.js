6e8eefe7f38ef4c56253a6ea23397526fa6ff50e
Eugene <eugeneayonga@gmail.com>
2022-10-25 08:09:49 +0300

    Refactored App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
-import Home from './components/Home';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
+// import Home from './components/Home';
+// import Login from './components/Login';
+// import SignUp from './components/SignUp';
 
 function App() {
   return (
@@ -12,10 +12,13 @@ function App() {
       <h1>Hello World</h1>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* render NavBar.js */}
+
+          <Route path="/" element={<NavBar />} />
+          {/* <Route path="/" element={<Home />} />
           <Route path="*" element={<NavBar />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signup" element={<SignUp />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
