c3edcad555190129fc5e62f282857859dc6dce5a
ChristopherChinia <chinia214@gmail.com>
2022-10-25 18:12:20 +0300

    [configuration]: installing @mui-icons for profile

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from '../Header/Header'
 import Router from "../../routers/Router"
 
 const Layout = () => {
   return (
     <>
+    <Header/>
 
     </>
   )
