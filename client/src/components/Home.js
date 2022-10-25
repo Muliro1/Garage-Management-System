f25fbbd057ab337dc1be658d5f7e572bab01af4c
Eugene <eugeneayonga@gmail.com>
2022-10-25 08:56:40 +0300

    Styled img, not displaying. Rerendered Home.js component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,10 +3,12 @@ import React from 'react'
 function Home() {
   return (
     <>
-      <img
-        alt="Image of a truck being serviced"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBKxL9QziNIeQQ3ZrG0anoUH89r9R0KVgH9g&usqp=CAU"
-      />
+      <div>
+        <img
+          alt="Truck being serviced"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBKxL9QziNIeQQ3ZrG0anoUH89r9R0KVgH9g&usqp=CAU"
+        />
+      </div>
     </>
   )
 }
