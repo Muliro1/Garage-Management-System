b30e06ea0b1fe1a2d2925e9bf265cf685a9623cb
muliro <mulirokhaemba@gmail.com>
2022-10-25 22:50:53 +0300

    Adding methods to the parts controller

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,2 +1,31 @@
+
+
+
+
+
+
+
 class PartsController < ApplicationController
+    skip_before_action :authorize, only: [:create, :index, :show]
+  
+    def create
+      part = Part.create!(part_params)
+      session[:part_id] = part.id
+      render json: part, status: :created
+    end
+
+    def index
+        parts = Part.all
+        render json: parts
+    end
+
+    def show
+      render json: @current_part
+    end
+  
+    private
+  
+    def part_params
+      params.permit(:name, :image, :price, :condition, :serial_number, :model, :id)
+    end
 end
