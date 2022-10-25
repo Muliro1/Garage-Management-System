3fa626dcac61bd77fee3379802aa585f75f4b136
Eugene <eugeneayonga@gmail.com>
2022-10-25 08:53:37 +0300

    Imported Home, Login and SignUp, built routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,28 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
-// import Home from './components/Home';
-// import Login from './components/Login';
-// import SignUp from './components/SignUp';
+import Home from './components/Home';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          {/* render NavBar.js */}
-
           <Route path="/" element={<NavBar />} />
-          {/* <Route path="/" element={<Home />} />
-          <Route path="*" element={<NavBar />} />
+
+          <Route path="/home" element={<Home />} />
+          
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} /> */}
+
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
