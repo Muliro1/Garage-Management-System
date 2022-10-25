48505a017ba89fb2887d9428c91c7c148ff89874
muliro <mulirokhaemba@gmail.com>
2022-10-25 21:41:14 +0300

    [:chore] adding the technicians table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_24_175305) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_25_182900) do
   create_table "admins", force: :cascade do |t|
     t.string "full_name"
     t.string "password"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "technicians", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
