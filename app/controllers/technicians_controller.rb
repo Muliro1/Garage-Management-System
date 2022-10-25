1677c723a7bfebf6cabdeb166208ebf6b2b187bd
muliro <mulirokhaemba@gmail.com>
2022-10-25 22:15:09 +0300

    [:chore] Adding more routes and a migration

--- app/controllers/technicians_controller.rb
+++ app/controllers/technicians_controller.rb
@@ -1,2 +1,31 @@
+
+
+
+
+
+
+
 class TechniciansController < ApplicationController
+    skip_before_action :authorize, only: :create
+  
+    def create
+      technician = Technician.create!(technician_params)
+      session[:technician_id] = technician.id
+      render json: technician, status: :created
+    end
+
+    def index
+        technicians = Technician.all
+        render json: technicians
+    end
+
+    def show
+      render json: @current_technician
+    end
+  
+    private
+  
+    def technician_params
+      params.permit(:username, :password, :password_confirmation, :id)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_25_184247) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_25_191239) do
   create_table "admins", force: :cascade do |t|
     t.string "full_name"
     t.string "password"
@@ -33,6 +33,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_25_184247) do
   create_table "technicians", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "email"
+    t.integer "telephone"
+    t.integer "rating"
+    t.integer "description"
   end
 
   create_table "users", force: :cascade do |t|
