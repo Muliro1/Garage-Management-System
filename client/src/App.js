515cb9a4ac0da3f8e61763423dfb4a431201de88
Eugene <eugeneayonga@gmail.com>
2022-10-25 08:11:17 +0300

    Added PBMW title

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import NavBar from './components/NavBar';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>PBMW</h1>
       <BrowserRouter>
         <Routes>
           {/* render NavBar.js */}
