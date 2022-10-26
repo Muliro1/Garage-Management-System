61430439f3190dde77484e9ea18ccc7c7b6ddb5e
ChristopherChinia <chinia214@gmail.com>
2022-10-26 09:31:01 +0300

    [setup]:creating a json database to mimick API

--- client/src/routers/Router.jsx
+++ client/src/routers/Router.jsx
@@ -3,8 +3,8 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from '../components/Home/Home'
 import Sidebar from '../components/SideBar/Sidebar'
 import Technicians from '../components/Technicians/Technicians'
-// import Cars from '../components/Cars/Cars'
-// import Parts from '../components/Parts/Parts'
+import Cars from '../components/Cars/Cars'
+import Parts from '../components/Parts/Parts'
 
 const Router = () => {
   return (
@@ -13,8 +13,8 @@ const Router = () => {
     <Routes>
     <Route path="/" element ={<Home />}/> 
     <Route path="/technicians" element={<Technicians/>}/> 
-    {/* <Route path="/cars" element={<Cars/>}/>  */}
-    {/* <Route path="/parts" element={<Parts/>}/>  */}
+    <Route path="/cars" element={<Cars/>}/> 
+    <Route path="/parts" element={<Parts/>}/> 
     </Routes>
     </Sidebar>
   </BrowserRouter>
