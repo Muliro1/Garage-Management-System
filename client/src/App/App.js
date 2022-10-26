fc6c384b73d6c3a42eee260eba0ef9c173a897fa
ChristopherChinia <chinia214@gmail.com>
2022-10-26 12:18:33 +0300

    [configuration]: renaming layout to user

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Layout from '../components/Layout/Layout'
+import User from '../components/User/User'
 
 const App = () => {
   return (
-    <Layout/>
+    <User/>
   )
 }
 
