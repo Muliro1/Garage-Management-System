ab5b47af81abf79b3923620c47b88592b8249a08
ChristopherChinia <chinia214@gmail.com>
2022-10-26 08:38:57 +0300

    [setup]: creating a caritem component

---
+++ client/src/components/PartItem/PartItem.jsx
@@ -0,0 +1,54 @@
+import React,{useState} from 'react'
+import {AiFillCar, AiFillSetting} from "react-icons/ai"
+import {RiTimerFlashFill} from "react-icons/ri"
+import Overview from '../OverView/Overview';
+import "./partItem.css"
+
+const PartItem = ({part}) => {
+    const {name, image, speed, price,model,automatic}= part;
+    const [show, setShow] = useState(false);
+    const [item, setItem] = useState();
+  return (
+    <>
+    <div className="car__item">
+    <div className="car__img">
+    <img src={image ? image : "https://via.placeholder.com/400"} alt={name}/>
+    </div>
+
+    <div className="car__item-content">
+      <h4 className="section__title">{name}</h4>
+      <h6 className="rent__price">
+        ${price}.00 <span>/ VAT</span>
+      </h6>
+
+      <div className="car__item-info">
+        <span>
+          <AiFillCar/> {model}
+        </span>
+        <span>
+          <AiFillSetting/> {automatic}
+        </span>
+        <span>
+          <RiTimerFlashFill/> {speed}
+        </span>
+      </div>
+        <div className='car__btn-action'>
+        <button className="car__btn-rent">
+        Buy Now
+      </button>
+      <button className="car__btn-details"
+        onClick={() => {
+            setShow(true);
+             setItem(part);
+         }}
+      >
+        Details
+      </button>
+        </div>
+    </div>
+  </div>
+        <Overview show={show} part={item} onClose={() => setShow(false)} />
+        </>
+  )
+}
+export default PartItem
