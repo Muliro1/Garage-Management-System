db31346a69804f8c010ec759144b341b229f0e3d
Eugene <eugeneayonga@gmail.com>
2022-10-27 18:06:15 +0300

    Completed Home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,16 @@
-import React from "react"
+import React from "react";
 
 const Home = () => {
   return (
     <>
-      <section className='hero'>
-        <h1>Welcome To Home Page</h1>
+      <section className='home'>
+        <div className="home-welcome">
+          {/* <img src="https://wallpaperaccess.com/full/1291276.jpg" alt="Eighteen wheeler Truck"/> */}
+          <h1>Afrika's No.1 Truck Servicing Provider<span><br></br>Join our Platform!</span></h1>
+          {/* <h3>Sign up today!</h3> */}
+        </div>
       </section>
+
     </>
   )
 }
