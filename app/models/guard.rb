9c0639bf5a42fead6e3b4aaa40c3c6e6a74e601f
muliro <mulirokhaemba@gmail.com>
2022-10-27 22:22:29 +0300

    [:chore] Adding auth validations

--- app/models/guard.rb
+++ app/models/guard.rb
@@ -7,4 +7,8 @@
 
 class Guard < ApplicationRecord
     has_secure_password
+
+    validates :full_name, presence: true, uniqueness: true
+    validates :password, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
 end
--- app/models/technician.rb
+++ app/models/technician.rb
@@ -6,6 +6,9 @@
 
 class Technician < ApplicationRecord
     has_secure_password
+    validates :name, presence: true, uniqueness: true
+    validates :password, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
 
 
     belongs_to :admin, class_name: "Technician", optional: true
