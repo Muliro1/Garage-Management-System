8a779ebbcd772906189a7397ce9d13c59b2cb853
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-10-27 17:01:57 +0300

    creates additional components

--- client/src/Components/Servicing.js
+++ client/src/Components/Servicing.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import ServicingCard from './ServicingCard';
+import ServicingImage from './ServicingImage';
+
 
 const Servicing = () => {
-    return (
-        <div>
-            <h1>Servicing vehicles</h1>
-        </div>
+    const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(`https://pixab.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, [term]);
+
+  return (
+    <div className="container mx-auto">
+      <ServicingImage searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
+
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ServicingCard key={image.id} image={image} />
+        ))}
+      </div>}
+    </div>
+        
+       
     );
-};
+}
 
 export default Servicing;
