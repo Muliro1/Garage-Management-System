37d2ebc67e13c791333bef6af2ba9303bf895751
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-10-28 14:46:17 +0300

    updates files

--- client/src/Components/ImageRepair.js
+++ client/src/Components/ImageRepair.js
@@ -1,24 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const ImageRepair = ({ searchText }) => {
-  const [text, setText] = useState('');
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    searchText(text);
-  }
+const ImageRepair = ({ image }) => {
+  // const tags = image.tags.split(',');
 
   return (
-    <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
-      <form onSubmit={onSubmit} className="w-full max-w-sm">
-        <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
-        <input onChange={e => setText(e.target.value)} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search Image Term..." />
-        <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
-      Search
-      </button>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src={image.image} alt="" className="w-full"/>
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by {image.name}
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            {image.email}
+          </li>
+          <li>
+            <strong>Downloads: </strong>
+            {image.speciality}
+          </li>
+          <li>
+            <strong>Likes: </strong>
+            {image.rating}
+          </li>
+        </ul>
       </div>
-      </form>
-		</div>
+      <div className="px-6 py-4">
+        {/* {tags.map((tag, index) => (
+          <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #{tag}
+        </span>
+        ))} */}
+      </div>
+    </div>
   )
 }
 
