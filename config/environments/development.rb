90117075ba9642e9307b1d9d3e73aadede2553a5
muliro <mulirokhaemba@gmail.com>
2022-10-28 04:48:30 +0300

    [:chore] emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,11 +42,11 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-  
+
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'example.com',
+  domain:               'gmail.com',
   user_name:            '<username>',
   password:             '<password>',
   authentication:       'plain',
