76243e5543d512d9cdecb3c83494042f69227ec7
Eugene <eugeneayonga@gmail.com>
2022-10-29 09:09:04 +0300

    Completed Services component

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -1,378 +1,95 @@
-// import React from "react"
+import React from "react"
 
-// const Services = () => {
-//   return (
-//     <>
-//       <section className='hero'>
-//         {/* <h1>Welcome To Services Page</h1> */}
-//       </section>
-//     </>
-//   )
-// };
-
-
-// export default Services;
-
-
-
-
-// Build a fully functional garage services component using React
-// The title should be GARAGE SERVICES
-// The component should have a list of services
-// The list should have the following services: Oil Change, Tire Rotation, Brake Repair, Engine Repair, Transmission Repair, Battery Replacement, Air Conditioning Repair, Electrical Repair, and Diagnostic Services
-// The component should have a button that says "Schedule Service"
-
-
-// import React from "react";
-
-// const Services = () => {
-//   return (
-//     <>
-//       <section className='services'>
-//         <h1>Our Services</h1>
-//         <ul>
-//           <li>Oil Change</li>
-//           <li>Tire Rotation</li>
-//           <li>Brake Repair</li>
-//           <li>Engine Repair</li>
-//           <li>Transmission Repair</li>
-//           <li>Battery Replacement</li>
-//           <li>Air Conditioning Repair</li>
-//           <li>Electrical Repair</li>
-//           <li>Diagnostic Services</li>
-//         </ul>
-//       </section>
-
-//       <section className='schedule'>
-//         <button type="submit" placeholder="Schedule Service" className="btn-schedule">Schedule Service</button>
-//       </section>
-//     </>
-//   )
-// };
-
-
-// export default Services;
-
-
-
-
-
-
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-// import Button from '@mui/material/Button';
-// import CameraIcon from '@mui/icons-material/PhotoCamera';
-import Card from '@mui/material/Card';
-// import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-// import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-// import Link from '@mui/material/Link';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-// function Copyright() {
-//   return (
-//     <Typography variant="body2" color="text.secondary" align="center">
-//       {'Copyright © '}
-//       <Link color="inherit" href="https://mui.com/">
-//         Your Website
-//       </Link>{' '}
-//       {new Date().getFullYear()}
-//       {'.'}
-//     </Typography>
-//   );
-// }
-
-const cards = [1, 2, 3];
-
-const theme = createTheme();
-
-export default function Album() {
+const Services = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-          {/* <CameraIcon sx={{ mr: 2 }} /> */}
-          {/* <Typography variant="h6" color="inherit" noWrap>
-            Album layout
-          </Typography> */}
-        </Toolbar>
-      </AppBar>
-      <main>
-        {/* Hero unit */}
-        <Box
-          sx={{
-            bgcolor: 'background.paper',
-            pt: 4,
-            pb: 2,
-          }}
-        >
-          <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="text.primary"
-              gutterBottom
-            >
-              Our Services
-            </Typography>
-            {/* <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Something short and leading about the collection below—its contents,
-              the creator, etc. Make it short and sweet, but not too short so folks
-              don&apos;t simply skip over it entirely.
-            </Typography> */}
-            {/* <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >
-              <Button variant="contained">Main call to action</Button>
-              <Button variant="outlined">Secondary action</Button>
-            </Stack> */}
-          </Container>
-        </Box>
-        <Container sx={{ py: 2 }} maxWidth="md">
-          {/* End hero unit */}
-          <Grid container spacing={3}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      // 16:9
-                      pt: '5%',
-                    }}
-                    image="https://media.istockphoto.com/photos/refueling-and-pouring-oil-quality-into-the-engine-motor-car-and-picture-id1212092642?k=20&m=1212092642&s=612x612&w=0&h=utkQXnp63UofnEfRZrjnlGLsJ8SkUEXNOvbPd0KGmcY="
-                    alt="random"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Oil change
-                    </Typography>
-                    {/* <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
-                    </Typography> */}
-                  </CardContent>
-                  {/* <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions> */}
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
+    <>
+      <section className='services'>
+        
+        <div className="services-cards">
 
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      // 16:9
-                      pt: '5%',
-                    }}
-                    image="https://cdn.wallpapersafari.com/79/32/sCMlFa.jpg"
-                    alt="truck engine"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Engine Repair
-                    </Typography>
-                    {/* <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
-                    </Typography> */}
-                  </CardContent>
-                  {/* <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions> */}
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
+        <h1>OUR SERVICES</h1>
 
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      // 16:9
-                      pt: '5%',
-                    }}
-                    image="https://i0.wp.com/www.cardiagnosticsa.co.za/wp-content/uploads/2022/08/ANCEL-HD3400-Truck-Diagnostic-Tool-Display-D2A.jpg?resize=700%2C700&ssl=1"
-                    alt="diagnostic services"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Diagnostic Services
-                    </Typography>
-                    {/* <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
-                    </Typography> */}
-                  </CardContent>
-                  {/* <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions> */}
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
+          <div class="responsive">
+            <div class="gallery">
+              <a target="_blank" href="img_5terre.jpg">
+              <img src="https://media.istockphoto.com/photos/refueling-and-pouring-oil-quality-into-the-engine-motor-car-and-picture-id1212092642?k=20&m=1212092642&s=612x612&w=0&h=utkQXnp63UofnEfRZrjnlGLsJ8SkUEXNOvbPd0KGmcY=" alt="Oil Change"/>
+              </a>
+              <div class="desc">Oil Change</div>
+            </div>
+          </div>
 
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      // 16:9
-                      pt: '5%',
-                    }}
-                    image="https://media.istockphoto.com/photos/cropped-view-of-mechanic-adjusting-assembled-disc-brakes-with-brake-picture-id1195719256?k=20&m=1195719256&s=612x612&w=0&h=qIym5bJ5ynek7eiyx68TbYXtQKEa2oTZ14UkG_KWcyo="
-                    alt="brake repair"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Brake Repair
-                    </Typography>
-                    {/* <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
-                    </Typography> */}
-                  </CardContent>
-                  {/* <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions> */}
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
+          <div class="responsive">
+            <div class="gallery">
+              <a target="_blank" href="img_5terre.jpg">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIHpKrYBi9prXkHSqVsm8aHbHk0vnz5Qe2PgZmdV3wLovfnkWdXyrUM5atiNXqkB6w7AM&usqp=CAU" alt="Truck Engine"/>
+              </a>
+              <div class="desc">Engine Repair</div>
+            </div>
+          </div>
 
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      // 16:9
-                      pt: '5%',
-                    }}
-                    image="https://www.researchgate.net/profile/Magnus-Mackaldener/publication/266049724/figure/fig2/AS:349263021264897@1460282201866/An-example-of-a-heavy-truck-gear-transmission-A-split-range-gearbox-from-Scania.png"
-                    alt="transmission system"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Transmission Repair
-                    </Typography>
-                    {/* <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
-                    </Typography> */}
-                  </CardContent>
-                  {/* <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions> */}
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
+          <div class="responsive">
+            <div class="gallery">
+              <a target="_blank" href="img_5terre.jpg">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbjWaE-LCe-yiAAbQrXmgd5E3xg2CdtsPMxwvr6lbmBSgrEaUrvYkaLn8hsISWwQvmWbI&usqp=CAU" alt="Truck Diagnostic"/>
+              </a>
+              <div class="desc">Diagnostic Services</div>
+            </div>
+          </div>
 
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      // 16:9
-                      pt: '5%',
-                    }}
-                    image="https://www.tyrepress.com/wp-content/uploads/2018/08/nokian-R-Truck-sml.jpg"
-                    alt="tires"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Tire Rotation
-                    </Typography>
-                    {/* <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
-                    </Typography> */}
-                  </CardContent>
-                  {/* <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions> */}
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
+          <div class="responsive">
+            <div class="gallery">
+              <a target="_blank" href="img_5terre.jpg">
+              <img src="https://media.istockphoto.com/photos/cropped-view-of-mechanic-adjusting-assembled-disc-brakes-with-brake-picture-id1195719256?k=20&m=1195719256&s=612x612&w=0&h=qIym5bJ5ynek7eiyx68TbYXtQKEa2oTZ14UkG_KWcyo=" alt="Brake Disc"/>
+              </a>
+              <div class="desc">Brake Repair</div>
+            </div>
+          </div>
 
-        </Container>
-      </main>
-      {/* Footer */}
-      {/* <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          Something here to give the footer a purpose!
-        </Typography>
-        <Copyright />
-      </Box> */}
-      {/* End footer */}
-    </ThemeProvider>
-  );
-}
+          <div class="responsive">
+            <div class="gallery">
+              <a target="_blank" href="img_5terre.jpg">
+              <img src="https://www.researchgate.net/profile/Magnus-Mackaldener/publication/266049724/figure/fig2/AS:349263021264897@1460282201866/An-example-of-a-heavy-truck-gear-transmission-A-split-range-gearbox-from-Scania.png" alt="Transmission System"/>
+              </a>
+              <div class="desc">Transmission Repair</div>
+            </div>
+          </div>
+
+          <div class="responsive">
+            <div class="gallery">
+              <a target="_blank" href="img_5terre.jpg">
+              <img src="https://i.roamcdn.net/hz/pi/listing-thumb-543w/ac5f551380e5c3944fd6caa8cc7047d4/-/horizon-files-prod/pi/picture/qqmx7vp/30c5e6f49a87de4aac4314f27e6ab2b1a097021b.jpg" alt="Tires"/>
+              </a>
+              <div class="desc">Tires Rotation</div>
+            </div>
+          </div>
+
+          <div class="responsive">
+            <div class="gallery">
+              <a target="_blank" href="img_5terre.jpg">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSGbedtQNwz4EUETdD7KE8O74E0cps4Ah5S7nQvH7B4TbYcun-Y21B_4dGn-KPcchnlA&usqp=CAU" alt="Electrical system"/>
+              </a>
+              <div class="desc">Electrical Repair</div>
+            </div>
+          </div>
+
+          <div class="responsive">
+            <div class="gallery">
+              <a target="_blank" href="img_5terre.jpg">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFPn2mJSANaLVtkuUs586PhVuSZGXzwLBbPiPtuJP92Z8rPvbG5HqN6kLH-_JP1fijSJQ&usqp=CAU" alt="Air Conditioning System"/>
+              </a>
+              <div class="desc">Air Conditioning Repair</div>
+            </div>
+          </div>
+          
+
+          <div class="clearfix"></div>
+            
+        </div>
+
+      </section>
+    </>
+  )
+};
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default Services;
