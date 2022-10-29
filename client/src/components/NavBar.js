7e388cfcffca7d15d96349f3ade4009253ab5e93
Eugene <eugeneayonga@gmail.com>
2022-10-29 09:33:14 +0300

    Deleted junk code

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,61 +1,4 @@
-// import React from 'react';
-// import { NavLink } from 'react-router-dom';
-
-
-// function NavBar() {
-//   return (
-    
-//     <div className='NavBar'>
-//         <h1>PBMW</h1>
-//         <NavLink to="/">
-//             <button className="button">Home</button>
-//         </NavLink>
-
-//         <NavLink to="/about">
-//             <button className="button">About</button>
-//         </NavLink>
-
-//         <NavLink to="/user">
-//             <button className="button">User</button>
-//         </NavLink>
-
-//         <NavLink to="/guard">
-//             <button className="button">Guard</button>
-//         </NavLink>
-
-//         <NavLink to="/admin">
-//             <button className="button">Admin</button>
-//         </NavLink>
-
-//         <NavLink to="/technician">
-//             <button className="button">Technician</button>
-//         </NavLink>
-
-//         <NavLink to="/contact">
-//             <button className="button">Contact</button>
-//         </NavLink>
-
-//         <NavLink to="/login">
-//             <button className="button">Login</button>
-//         </NavLink>
-
-//         <NavLink to="/signup">
-//             <button className="button">Sign Up</button>
-//         </NavLink>
-//     </div>
-    
-//   );
-// }
-
-
-// export default NavBar;
-
-
-
-
-
 import React, { useState } from "react"
-// import "./navbar.css"
 import { Link } from "react-router-dom"
 import { FaBars } from "react-icons/fa"
 import { ImCross } from "react-icons/im"
@@ -68,10 +11,6 @@ const Navbar = () => {
         <div className="logo">
           <h1>PBMW</h1>
         </div>
-        {/*
-        if large screen ma xa bhane Mobile add huxa
-        if mobile screen ma xa bhane nav-links-mobile add huxa
-        */}
         <ul className={Mobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}>
           
           <Link to='/' className='home'>
@@ -107,16 +46,13 @@ const Navbar = () => {
           </Link>
 
         </ul>
-        {/* 
-        whenever we click on button = setMobile(!Mobile) ==  is mobile oppsite to setMobile 
-        */}
         <button className='mobile-menu-icon' onClick={() => setMobile(!Mobile)}>
           {Mobile ? <ImCross /> : <FaBars />}
         </button>
       </nav>
     </>
   )
-}
+};
 
 
 export default Navbar;
