b974be492b728082ed186fc1fe6799c0f3e51e9e
Eugene <eugeneayonga@gmail.com>
2022-10-29 09:30:03 +0300

    Refactored junk code in Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,9 +5,7 @@ const Home = () => {
     <>
       <section className='home'>
         <div className="home-welcome">
-          {/* <img src="https://wallpaperaccess.com/full/1291276.jpg" alt="Eighteen wheeler Truck"/> */}
           <h1>Afrika's No.1 Truck Servicing Provider<span><br></br>Join our Platform!</span></h1>
-          {/* <h3>Sign up today!</h3> */}
         </div>
       </section>
 
