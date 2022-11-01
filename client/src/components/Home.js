f4021dd6f984103a908012f2d434affe15abd7d1
Eugene <eugeneayonga@gmail.com>
2022-11-01 08:41:42 +0300

    Typo editing

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     <>
       <section className='home'>
         <div className="home-welcome">
-          <h1>Afrika's No.1 Truck Servicing Provider<span><br></br>Join our Platform!</span></h1>
+          <h1>Afrika's No.1 Vehicle Servicing Provider<span><br></br>Join our Platform!</span></h1>
         </div>
       </section>
 
