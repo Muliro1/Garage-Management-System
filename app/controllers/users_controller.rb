3ef75b73c627145145661e680118bd79354cefbf
Muliro <mulirokhaemba@gmail.com>
2023-08-31 03:40:09 +0300

    user update route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,15 @@ class UsersController < ApplicationController
         render json: { error: "User not found" }, status: :not_found
       end
   end
-  
+  def update
+    user = User.find_by(id: params[:id])
+    if user
+      user.update(user_params)
+      render json: user
+    else
+      render json: { error: "user not found" }, status: :not_found
+    end 
+  end
     private
   
     def user_params
